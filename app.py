e0bff1c8a2cd245c56e12036ab23fef5841b2df9
Moisés Briseño Estrello <Moibe@users.noreply.huggingface.co>
2023-12-30 19:43:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import gradio as gr
 def greet(input_image):
     return input_image
 
-iface = gr.Interface(greet, gr.Image(height=200, width=200), "image")
+iface = gr.Interface(greet, gr.Video(height=200, width=200), "video")
 iface.launch()
