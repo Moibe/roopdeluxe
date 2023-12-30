40e85ae2cab074f70c157e7cf361d2663d24162e
Moisés Briseño Estrello <Moibe@users.noreply.huggingface.co>
2023-12-30 19:38:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import gradio as gr
 def greet(name):
     return "OK"
 
-iface = gr.Interface(fn=greet, gr.Image(height=200, width=200), "image")
+iface = gr.Interface(greet, gr.Image(height=200, width=200), "image")
 iface.launch()
