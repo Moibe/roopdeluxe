f2c924563688a911ec0e1dd3ca006cbe5daf3dbe
Moibe <moi_estrello@hotmail.com>
2024-01-07 00:39:08 -0600

    Add PIL conversion

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 import gradio as gr
+from PIL import Image
 
 def greet(input1, input2):
     print("Imprimiendo en Consola")
     print("Ésto es input1: ", input1)
     print("Ésto es input2: ", input2)
+
+    #Aquí voy a poner como lo maneja roop en hf.
+    #https://huggingface.co/spaces/ezioruan/roop/blob/main/app.py
+
+    source_path = "input.jpg"
+    target_path = "target.jpg"
+
+    source_image = Image.fromarray(input1)
+    source_image.save(source_path)
+    target_image = Image.fromarray(input2)
+    target_image.save(target_path)
+
+    print("source_path: ", source_path)
+    print("target_path: ", target_path)
+
+
     return input1
 
 #def carga_consola():
@@ -22,7 +39,7 @@ def greet(input1, input2):
 #     btn.click(greet, inputs=[gr.Image(), gr.Image()], outputs="image")
 
 demo = gr.Interface(
-fn=greet, inputs=[gr.Video(), gr.Video()], outputs="video"
+fn=greet, inputs=[gr.Image(), gr.Image()], outputs="image"
 )
 
 demo.launch()
