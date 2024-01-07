f4ea82482e815af3a2107cf5cc923cdcd2d08304
Moibe <moi_estrello@hotmail.com>
2024-01-06 21:21:06 -0600

    Back to basic

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import gradio as gr
 
 def greet(input_video):
     print("Hola")
-    video_origen = input_video.value
-    print(video_origen)
+    #video_origen = input_video.value
+    #print(video_origen)
     # Guardar el video en un archivo
-    video_origen.save("video_guardado.mp4")
+    #video_origen.save("video_guardado.mp4")
     return input_video
 
 iface = gr.Interface(greet, gr.Video(height=200, width=200), "video")
