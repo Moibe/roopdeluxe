b1b5fc57e9c94208dcaa049cd8c1ce55e650cda9
Moibe <moi_estrello@hotmail.com>
2024-01-06 23:38:18 -0600

    Fix Launch

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ def greet(input_video):
     print("Imprimiendo en Consola")
     return input_video
 
-def carga_consola():
-    return "Hola Mundo"
+#def carga_consola():
+#    return "Hola Mundo"
 
 #iface = gr.Interface(greet, gr.Video(height=200, width=200), "video")
 #gr.show()
 
 with gr.Blocks() as demo:
+    print("Imprimiendo en Arranque...")
     with gr.Row():
         input = gr.Video()
         output = gr.Video()
@@ -20,4 +21,4 @@ with gr.Blocks() as demo:
     #consola = gr.Textbox()
     #demo.load(carga_consola, None, consola)
 
-demo.queue().launch()
+demo.launch()
