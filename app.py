1bc6f91ad3d490efde35bb04032195c71b0d1585
Moibe <moi_estrello@hotmail.com>
2024-01-06 21:12:00 -0600

    Testing video object and console

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import gradio as gr
 
 def greet(input_video):
+    print("Hola")
+    video_origen = input_video.value
+    print(video_origen)
+    # Guardar el video en un archivo
+    video_origen.save("video_guardado.mp4")
     return input_video
 
 iface = gr.Interface(greet, gr.Video(height=200, width=200), "video")
