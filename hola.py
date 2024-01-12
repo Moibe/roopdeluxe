09c6157f61b157c632a551909bbc80dfa7505a23
Moibe <moi_estrello@hotmail.com>
2024-01-11 19:43:09 -0600

    Fix nueva_var

--- hola.py
+++ hola.py
@@ -11,5 +11,5 @@ print("Esto es args: ", args)
 print("Hola desde Hola.py")
 print("El target path recibido es: ", args.target_path)
 
-nueva_var = args.target_path
-print("La nnueva var es: ", nueva_var)
+#nueva_var = args.target_path
+#print("La nnueva var es: ", nueva_var)
