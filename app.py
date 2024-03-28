8780e2907c6ae0db3f1d3526147a07a84c0cfe47
Moibe <moi_estrello@hotmail.com>
2024-03-27 23:53:15 -0600

    Fix line path_parts = path_video.split

--- app.py
+++ app.py
@@ -61,8 +61,8 @@ def perform(input1, input2, input3):
     #else:
         #Para HuggingFace
         #Creo que no va en imagen.
-        # print("La plataforma en la que basaremos la división es HuggingFace.")
-        # path_parts = path_video.split("/")
+        print("La plataforma en la que basaremos la división es HuggingFace.")
+        path_parts = path_video.split("/")
 
     #Aquí obtendremos nom_video
     #Creo no va en imagen
